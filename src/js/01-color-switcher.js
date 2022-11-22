7dface935c1a987fae831a0d245ee0ddfdbf7d85
Olena <newsroommy@gmail.com>
2022-11-22 16:58:15 +0200

    01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,25 @@
 const refs = {
   btnStartRefs: document.querySelector('[data-start]'),
-
   btnStopRefs: document.querySelector('[data-stop]'),
+  bodyRef: document.body,
 };
 
-refs.btnStartRefs.addEventListener('click', onChangeColorBody);
-refs.btnStopRefs.addEventListener('click', onStopChangeColorBody);
+refs.btnStartRefs.addEventListener('click', onChangeColorClick);
+refs.btnStopRefs.addEventListener('click', onStopChangeColorClick);
 
 console.log(refs);
+
+let timerId = null;
+
+function onChangeColorClick() {
+  timerId = setInterval(() => {
+    refs.bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeColorClick() {
+  clearInterval(timerId);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
