ec83fe7d1234aeaac2e527dd92cbf9ab6927572d
OllBond <107562787+OllBond@users.noreply.github.com>
2022-11-22 13:01:42 +0000

    Deploying to gh-pages from @ OllBond/goit-js-hw-09@d02935242b49ac38a7df3fe3af1ffd741fe121fc 🚀

---
+++ 01-color-switcher.447c6340.js
@@ -0,0 +1,2 @@
+const t={btnStartRefs:document.querySelector("[data-start]"),btnStopRefs:document.querySelector("[data-stop]")};t.btnStartRefs.addEventListener("click",onChangeColorBody),t.btnStopRefs.addEventListener("click",onStopChangeColorBody),console.log(t);
+//# sourceMappingURL=01-color-switcher.447c6340.js.map
---
+++ 01-color-switcher.86bf5ee0.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStartRefs:document.querySelector("[data-start]"),btnStopRefs:document.querySelector("[data-stop]")};t.btnStartRefs.addEventListener("click",onChangeColorBody),t.btnStopRefs.addEventListener("click",onStopChangeColorBody),console.log(t)}();
+//# sourceMappingURL=01-color-switcher.86bf5ee0.js.map
