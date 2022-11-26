6ba9134ffdd93928406f81742c86a2eef9feb56b
Olena <newsroommy@gmail.com>
2022-11-26 16:36:26 +0200

    02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,5 +13,18 @@ class Timer {
 
   stop() {}
 }
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
 // на старті програми зробити екзепляр класу
 const timer = new Timer('#datetime-picker');
+
+// ініціалізація бібліотеки
+flatpickr('#datetime-picker', options);
